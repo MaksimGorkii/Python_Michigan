5def9e02603f6e446a05c4b3446984c5328d8afd
MaksimGorkii <33204237+MaksimGorkii@users.noreply.github.com>
2020-01-14 14:20:18 -0500

    Add files via upload
    Final capstone

---
+++ c5/gmane/gmane/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Year','umich.edu','indiana.edu','berkeley.edu','ucdavis.edu','mac.com','cam.ac.uk','gmail.com','uct.ac.za','yale.edu','ufp.pt'],
+['2005',57,12,12,11,12,7,10,14,5,10],
+['2006',317,107,95,87,85,87,77,64,63,56]
+];
